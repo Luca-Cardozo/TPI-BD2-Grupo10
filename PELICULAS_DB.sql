72384f97b2e221bc69a8ff1afbfca862fdebf4ba
Angeles-Albornoz1 <angelesbeatrizalbornoz@gmail.com>
2026-05-16 16:30:16 -0300

    Agregue tablas Usuarios, Suscripciones, UsuariosxSuscripciones y Resenias
    Creadas las tablas, con su tipo de dato y restricciones,

--- PELICULAS_DB.sql
+++ PELICULAS_DB.sql
@@ -62,3 +62,60 @@ CREATE TABLE PeliculasxGeneros (
     IdGenero INT NOT NULL,
     CONSTRAINT PK_PeliculasxGeneros PRIMARY KEY (Id)
 );
+
+
+--USUARIOS
+GO
+CREATE TABLE Usuarios (
+    IdUsuario INT NOT NULL IDENTITY(1,1),
+    Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
+    DNI VARCHAR(10) NOT NULL UNIQUE,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    Password VARCHAR(100) NOT NULL,
+    Telefono VARCHAR(25) NULL,
+    EsAdmin BIT NOT NULL DEFAULT 0,
+
+    CONSTRAINT PK_Usuarios PRIMARY KEY (IdUsuario)
+    );
+    GO
+    
+    --SUSCRIPCIONES
+    CREATE TABLE Suscripciones (
+    IdSuscripcion INT NOT NULL IDENTITY(1,1),
+    Descripcion VARCHAR(50) NOT NULL,
+    PrecioMensual MONEY NOT NULL,
+
+    CONSTRAINT PK_Suscripciones PRIMARY KEY (IdSuscripcion)
+    );
+
+    GO
+    --USUARIOSxSUSCRIPCIONES
+    CREATE TABLE UsuariosxSuscripciones (
+    Id INT NOT NULL IDENTITY(1,1),
+    IdUsuario INT NOT NULL,
+    IdSuscripcion INT NOT NULL,
+    FechaAlta DATE NOT NULL,
+    FechaBaja DATE NULL,
+    Activo BIT NOT NULL DEFAULT 1,
+
+    CONSTRAINT PK_UsuariosxSuscripciones PRIMARY KEY (Id),
+    CONSTRAINT CHK_UsuariosxSuscripciones_FechaBaja
+        CHECK (FechaBaja IS NULL OR FechaBaja >= FechaAlta)
+
+    );
+    GO
+    
+    --RESEÑAS
+    CREATE TABLE Resenias (
+    IdResenia INT NOT NULL IDENTITY(1,1),
+    IdUsuario INT NOT NULL,
+    IdPelicula INT NOT NULL,
+    Comentario VARCHAR(1000) NOT NULL,
+    Calificacion INT NOT NULL,
+    Fecha DATE NOT NULL,
+
+    CONSTRAINT PK_Resenias PRIMARY KEY (IdResenia),
+    CONSTRAINT CHK_Resenias_Calificacion CHECK (Calificacion BETWEEN 1 AND 10)
+);
+GO
